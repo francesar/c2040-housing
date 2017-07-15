c2ee035596ac81a0ce4fc9a465e37d82c7ee012a
cesaribarra10 <cfi2103@columbia.edu>
2017-07-15 10:47:09 -0700

    Initial Commit

---
+++ housing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  housing
+//
+//  Created by Cesar Ibarra on 7/15/17.
+//  Copyright © 2017 Cesar Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
